21eef1771fa16040f6bcfe603783f3f929178674
KhyatiBhosle <56262867+KhyatiBhosle@users.noreply.github.com>
2021-04-15 15:25:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+document.querySelectorAll(".question")[0].addEventListener("click", function(){
+    document.querySelectorAll(".answer")[0].classList.toggle("collapse");
+    document.querySelectorAll(".q1")[0].classList.toggle("clicked");
+    document.querySelectorAll(".arrow")[0].classList.toggle("arrow-rotate");
+});
+
+document.querySelectorAll(".question")[1].addEventListener("click", function(){
+    document.querySelectorAll(".answer")[1].classList.toggle("collapse");
+    document.querySelectorAll(".q1")[1].classList.toggle("clicked");
+});
+
+document.querySelectorAll(".question")[2].addEventListener("click", function(){
+    document.querySelectorAll(".answer")[2].classList.toggle("collapse");
+    document.querySelectorAll(".q1")[2].classList.toggle("clicked");
+});
+
+document.querySelectorAll(".question")[3].addEventListener("click", function(){
+    document.querySelectorAll(".answer")[3].classList.toggle("collapse");
+    document.querySelectorAll(".q1")[3].classList.toggle("clicked");
+});
+
+document.querySelectorAll(".question")[4].addEventListener("click", function(){
+    document.querySelectorAll(".answer")[4].classList.toggle("collapse");
+    document.querySelectorAll(".q1")[4].classList.toggle("clicked");
+});
